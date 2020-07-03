bacd5948daaf750bbc153a0cffcee02415f02509
Tim <tim.de.klijn@sogeti.com>
2020-07-03 16:33:33 +0200

    Update

--- actions.py
+++ actions.py
@@ -5,8 +5,6 @@
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
 
-# This is a simple example for a custom action which utters "Hello World!"
-
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
